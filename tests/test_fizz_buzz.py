d085099c424ffbc9dc60d1f747df0035f33c4cb1
Salhi Abdo <salhiabdennour58@gmail.com>
2025-10-22 23:57:30 +0100

    refactoring

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -3,5 +3,8 @@ from src.fizz_buzz import fizz__buzz
 def test_function_is_return_fizz():
     assert fizz__buzz(3)=="fizz"
 
-def test_function_is_return_0():
+def test_function_is_return_buzz():
     assert fizz__buzz(5)=="buzz"
+
+def test_function_is_return_buzz_num_9():
+    assert fizz__buzz(9)=="fizz"
