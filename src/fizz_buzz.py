34ce3cd1d72f4e87907acd1ae34736ba169abd9f
Salhi Abdo <salhiabdennour58@gmail.com>
2025-10-22 23:50:18 +0100

    test function return fizz or buzz or return the same number

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,4 +1,7 @@
 def fizz__buzz(num:int) -> str | int:
     if num == 3:
      return "fizz"
+    if num == 5:
+     return "buzz"
+
     return num
--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -4,8 +4,10 @@ def test_function_is_return_fizz():
     assert fizz__buzz(3)=="fizz"
 
 def test_function_is_return_0():
-    assert fizz__buzz(0)==0
+    assert fizz__buzz(5)=="buzz"
 
+def test_function_is_return_8():
+    assert fizz__buzz(8) == 8
 
-def test_function_is_return_1():
-    assert fizz__buzz(1)==1
+def test_function_is_return_the_10():
+    assert fizz__buzz(10) == 10
